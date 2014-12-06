d6d1ae927a6e5dc4c38894121dd5fc345061d797
Andrey Borovoy <andriy_borovuy@ukr.net>
2014-12-06 12:20:46 +0200

    Lesson4 home task

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
-  validates :title, uniqueness: true, length: {minimum: 5, maximum: 100}
-  validates_presence_of :body
+  validates :title, uniqueness: true, presence: true, length: {minimum: 5, maximum: 100}
+  validates_presence_of :body, :user_id
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+
+  get 'sessions/logout'
+
+  get 'registration' => 'users#new'#, as: 'registration'
+  post 'users' => 'users#create'
+
+  post 'sessions' => 'sessions#create'
+
   resources :posts
 
   root 'posts#index'
